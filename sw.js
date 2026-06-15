6753d3f785c6e71ca30c58b998ff3c72b3062e0e
suzananageloficial-star <suzananagel.oficial@gmail.com>
2026-06-15 17:46:21 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Atualiza sozinho assim que um sw.js novo é detectado.
 
 // >>> Ao subir uma atualização do app, troque a data abaixo para forçar a renovação. <<<
-const CACHE = 'tiasu-2026-06-15-3';
+const CACHE = 'tiasu-2026-06-15-4';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting(); // ativa a nova versão na hora, sem esperar fechar o app
