af9fb9e1e65f9d6b30f9db109b73d01a6677aa26
suzananageloficial-star <suzananagel.oficial@gmail.com>
2026-06-23 16:29:52 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Atualiza sozinho assim que um sw.js novo é detectado.
 
 // >>> Ao subir uma atualização do app, troque a data abaixo para forçar a renovação. <<<
-const CACHE = 'tiasu-2026-06-20-firebase';
+const CACHE = 'tiasu-2026-06-22-fixes';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting(); // ativa a nova versão na hora, sem esperar fechar o app
