aed2892c606b25583e07bf3e66787ff4801e9133
suzananageloficial-star <suzananageloficial-star@users.noreply.github.com>
2026-09-15 16:25:27 -0300

    App Tia Su v15/09-c27

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Atualiza sozinho assim que um sw.js novo é detectado.
 
 // >>> Ao subir uma atualização do app, troque a data abaixo para forçar a renovação. <<<
-const CACHE = 'tiasu-v08-09-c26';
+const CACHE = 'tiasu-v15-09-c27';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting(); // ativa a nova versão na hora, sem esperar fechar o app
